2ea6216046e5211cef3bb03c31bcfbdc0db286d7
Vinícius Gomes <90351185+Vinnigs@users.noreply.github.com>
2023-10-30 13:31:03 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,51 +1,11 @@
-// const swiper = new Swiper(".mySwiper", {
-//     effect: "slide",
-//     slidesPerView: 2,
-//     speed: 3000,
-//     loop: true,
-//     loopedSlides: 9,
-//     spaceBetween: 32,
-//     autoplay: {
-//         "delay": 1000,
-//         "disableOnInteraction": false,
-
-//     },
-
-//     breakpoints: {
-//         // when window width is >= 320px
-//         580: {
-//           slidesPerView: 3,
-//           spaceBetween: 32,
-//         },
-//         740: {
-//           slidesPerView: 4,
-//           spaceBetween: 32,
-//         },
-//         900: {
-//           slidesPerView: 5,
-//           spaceBetween: 32,
-//         }
-//     }
-
-// });
 
 const swiper = new Swiper(".mySwiper", {
     effect: "slide",
     slidesPerView: 1,
-    // spaceBetween: 32,
-    // slideActiveClass: 'swiper-slide-active',
-    // centeredSlides: true,
-    // initialSlide: 0,
-    // slideToClickedSlide: true,
-    // speed: 3000,
+    
     loop: true,
     loopedSlides: 1,
-    // spaceBetween: 32,
-    // autoplay: {
-    //     "delay": 1000,
-    //     "disableOnInteraction": false,
 
-    // },
 
     navigation: {
       nextEl: '.swiper-button-next',
@@ -57,23 +17,6 @@ const swiper = new Swiper(".mySwiper", {
       type: 'bullets',
     },
 
-  
-
-    // breakpoints: {
-    //     // when window width is >= 320px
-    //     850: {
-    //       slidesPerView: 2,
-    //       spaceBetween: 32,
-    //     },
-    //     1250: {
-    //       slidesPerView: 3,
-    //       spaceBetween: 32,
-    //     },
-    //     // 900: {
-    //     //   slidesPerView: 5,
-    //     //   spaceBetween: 32,
-    //     // }
-    // }
 
 });
 
@@ -81,20 +24,9 @@ const swiper = new Swiper(".mySwiper", {
 const swiperPrincipal = new Swiper(".swiperPrincipal", {
     effect: "slide",
     slidesPerView: 1,
-    // spaceBetween: 32,
-    // slideActiveClass: 'swiper-slide-active',
-    // centeredSlides: true,
-    // initialSlide: 0,
-    // slideToClickedSlide: true,
-    // speed: 3000,
+    
     loop: true,
     loopedSlides: 1,
-    // spaceBetween: 32,
-    // autoplay: {
-    //     "delay": 1000,
-    //     "disableOnInteraction": false,
-
-    // },
 
     navigation: {
       nextEl: '.swiper-button-next',
@@ -107,22 +39,6 @@ const swiperPrincipal = new Swiper(".swiperPrincipal", {
     },
 
   
-
-    // breakpoints: {
-    //     // when window width is >= 320px
-    //     850: {
-    //       slidesPerView: 2,
-    //       spaceBetween: 32,
-    //     },
-    //     1250: {
-    //       slidesPerView: 3,
-    //       spaceBetween: 32,
-    //     },
-    //     // 900: {
-    //     //   slidesPerView: 5,
-    //     //   spaceBetween: 32,
-    //     // }
-    // }
 
 });
 
